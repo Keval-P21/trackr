dad4a1ccfff11999ae8df23fe3b2da566a20274b
Keval-P21 <keval.p21+github@gmail.com>
2022-07-31 13:00:20 +0100

    Feat Job Info form styled and moved to seperate componenet

--- client/src/components/jobs/JobDetails.js
+++ client/src/components/jobs/JobDetails.js
@@ -2,30 +2,15 @@ import React, { useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ApiClientService from '../../services/ApiClientService';
 import TodoSingle from '../todos/TodoSingle';
+import JobInfo from './JobInfo';
 
 function JobDetails({ jobs, setJobs, getUserJobs }) {
-  const [isDisabled, setIsDisabled] = useState(true);
   const jobId = useParams();
-  const titleRef = useRef(null);
-  const companyRef = useRef(null);
-  const salaryRef = useRef(null);
-  const locationRef = useRef(null);
-  const post_urlRef = useRef(null);
-  const descriptionRef = useRef(null);
-  const statusRef = useRef(null);
   const todoRef = useRef(null);
 
   if (!jobs) return <div>Loading</div>;
 
   const data = jobs.filter((job) => job._id === jobId.id);
-
-  function handleClick() {
-    setIsDisabled((current) => !current);
-  }
-
-  function refresh() {
-    window.location.reload();
-  }
 
   async function submitTodo(event) {
     const todoData = {
@@ -38,7 +23,6 @@ function JobDetails({ jobs, setJobs, getUserJobs }) {
         },
       ],
     };
-    // console.log(todoData);
     const newJob = await ApiClientService.editJob(todoData);
     setJobs((prevState) => {
       const newJobId = newJob._id;
@@ -48,18 +32,14 @@ function JobDetails({ jobs, setJobs, getUserJobs }) {
     event.target.reset();
   }
   async function deleteTodo(id) {
-    // const todoData = data[0];
     const todoData = data[0].todos;
-    // console.log('todo Data', todoData, id);
     const filteredTodo = todoData.filter((el) => el._id === id)[0];
     filteredTodo.active = false;
-    // console.log('FilteredTodo Active Change', filteredTodo);
     const updatedTodos = {
       ...data[0],
       todos: todoData,
     };
 
-    // console.log(updatedTodos);
     const newJob = await ApiClientService.editJob(updatedTodos);
     setJobs((prevState) => {
       const newJobId = newJob._id;
@@ -68,135 +48,11 @@ function JobDetails({ jobs, setJobs, getUserJobs }) {
     getUserJobs(2);
   }
 
-  async function handleSave() {
-    const editedData = {
-      ...data[0],
-      company: companyRef.current.value,
-      title: titleRef.current.value,
-      status: statusRef.current.value,
-      userId: '2',
-      location: locationRef.current.value,
-      salary: salaryRef.current.value,
-      post_url: post_urlRef.current.value,
-      interview: '',
-      description: descriptionRef.current.value,
-      notes: '',
-    };
-
-    const newJob = await ApiClientService.editJob(editedData);
-    setJobs((prevState) => {
-      const newJobId = newJob._id;
-      return [...prevState.filter((el) => el._id !== newJobId), newJob];
-    });
-    getUserJobs(2);
-    setIsDisabled((current) => !current);
-  }
-
   return jobs && data.length ? (
     <div>
       <div className='edit-cont'>
         <div>JobDetails</div>
-        <div className='form-box'>
-          {/* <form className='white'> */}
-          <div>
-            <fieldset>
-              <label htmlFor='title' className='c-form-label'>
-                Job Title* :
-              </label>
-              <input
-                type='text'
-                id='title'
-                className='c-form-input'
-                defaultValue={data[0].title}
-                disabled={isDisabled}
-                ref={titleRef}
-                required
-              />
-            </fieldset>
-          </div>
-          <div className='input-field col s12 m6'>
-            <label htmlFor='company'>Company* :</label>
-            <input
-              type='text'
-              id='company'
-              defaultValue={data[0].company}
-              disabled={isDisabled}
-              ref={companyRef}
-              required
-            />
-          </div>
-          <div className='input-field col s12 m6'>
-            <label htmlFor='salary'>Salary</label>
-            <input
-              type='text'
-              id='salary'
-              defaultValue={data[0].salary ? data[0].salary : ''}
-              disabled={isDisabled}
-              ref={salaryRef}
-            />
-          </div>
-          <div className='input-field col s12 m6'>
-            <label htmlFor='location'>Location</label>
-            <input
-              type='text'
-              id='location'
-              defaultValue={data[0].location ? data[0].location : ''}
-              disabled={isDisabled}
-              ref={locationRef}
-            />
-          </div>
-          <div className='input-field col s12 m6'>
-            <label htmlFor='post_url'>URL (Link)</label>
-            <input
-              type='text'
-              id='post_url'
-              defaultValue={data[0].post_url ? data[0].post_url : ''}
-              disabled={isDisabled}
-              ref={post_urlRef}
-            />
-          </div>
-          <div className='input-field col s12 '>
-            <label htmlFor='description'>Description</label>
-            <input
-              type='text'
-              id='description'
-              defaultValue={data[0].description ? data[0].description : ''}
-              disabled={isDisabled}
-              ref={descriptionRef}
-            />
-          </div>
-          <div className='input-field col s12 m6'>
-            <select
-              className='browser-default'
-              id='status'
-              defaultValue={`${data[0].status}`}
-              disabled={isDisabled}
-              required
-              ref={statusRef}
-            >
-              <option value='' disabled>
-                Application Status *
-              </option>
-              <option value='pending'>Pending</option>
-              <option value='applied'>Applied</option>
-              <option value='phone'>Phone</option>
-              <option value='onsite'>Onsite</option>
-              <option value='offer'>Offer</option>
-              <option value='rejected'>Rejected</option>
-            </select>
-          </div>
-          {isDisabled ? (
-            <>
-              <button onClick={handleClick}>Edit</button>
-            </>
-          ) : (
-            <>
-              <button onClick={refresh}>Cancel</button>
-              <button onClick={handleSave}>Save</button>
-            </>
-          )}
-          {/* </form> */}
-        </div>
+        <JobInfo jobs={jobs} setJobs={setJobs} getUserJobs={getUserJobs} />
       </div>
       <div>
         <form onSubmit={submitTodo}>
--- client/src/components/jobs/JobInfo.js
+++ client/src/components/jobs/JobInfo.js
@@ -1,10 +1,182 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import ApiClientService from '../../services/ApiClientService';
 
-function JobInfo() {
-  return (
+function JobInfo({ jobs, setJobs, getUserJobs }) {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const jobId = useParams();
+  const titleRef = useRef(null);
+  const companyRef = useRef(null);
+  const salaryRef = useRef(null);
+  const locationRef = useRef(null);
+  const post_urlRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const statusRef = useRef(null);
+
+  if (!jobs) return <div>Loading</div>;
+
+  const data = jobs.filter((job) => job._id === jobId.id);
+
+  function handleClick() {
+    setIsDisabled((current) => !current);
+  }
+
+  function refresh() {
+    window.location.reload();
+  }
+
+  async function handleSave() {
+    const editedData = {
+      ...data[0],
+      company: companyRef.current.value,
+      title: titleRef.current.value,
+      status: statusRef.current.value,
+      userId: '2',
+      location: locationRef.current.value,
+      salary: salaryRef.current.value,
+      post_url: post_urlRef.current.value,
+      interview: '',
+      description: descriptionRef.current.value,
+      notes: '',
+    };
+
+    const newJob = await ApiClientService.editJob(editedData);
+    setJobs((prevState) => {
+      const newJobId = newJob._id;
+      return [...prevState.filter((el) => el._id !== newJobId), newJob];
+    });
+    getUserJobs(2);
+    setIsDisabled((current) => !current);
+  }
+  return jobs && data.length ? (
     <>
-      <div>JobInfo</div>
+      <div className='form-box'>
+        <fieldset>
+          <label htmlFor='title' className='form-label'>
+            Job Title *
+          </label>
+          <input
+            type='text'
+            id='title'
+            className='form-input'
+            defaultValue={data[0].title}
+            disabled={isDisabled}
+            ref={titleRef}
+            required
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='company' className='form-label'>
+            Company *
+          </label>
+          <input
+            type='text'
+            id='company'
+            className='form-input'
+            defaultValue={data[0].company}
+            disabled={isDisabled}
+            ref={companyRef}
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='status' className='form-label'>
+            Application Status *
+          </label>
+          <select
+            id='status'
+            className='form-input'
+            defaultValue={`${data[0].status}`}
+            disabled={isDisabled}
+            required
+            ref={statusRef}
+          >
+            <option value='' disabled>
+              Application Status *
+            </option>
+            <option value='pending'>Pending</option>
+            <option value='applied'>Applied</option>
+            <option value='phone'>Phone</option>
+            <option value='onsite'>Onsite</option>
+            <option value='offer'>Offer</option>
+            <option value='rejected'>Rejected</option>
+          </select>
+        </fieldset>
+        <fieldset>
+          <label htmlFor='salary' className='form-label'>
+            Salary
+          </label>
+          <input
+            type='text'
+            id='salary'
+            className='form-input'
+            defaultValue={data[0].salary ? data[0].salary : ''}
+            disabled={isDisabled}
+            ref={salaryRef}
+          />
+        </fieldset>
+
+        <fieldset>
+          <label htmlFor='location' className='form-label'>
+            Location
+          </label>
+          <input
+            type='text'
+            id='location'
+            className='form-input'
+            defaultValue={data[0].location ? data[0].location : ''}
+            disabled={isDisabled}
+            ref={locationRef}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='post_url' className='form-label'>
+            URL (Link)
+          </label>
+          <input
+            type='text'
+            id='post_url'
+            className='form-input'
+            defaultValue={data[0].post_url ? data[0].post_url : ''}
+            disabled={isDisabled}
+            ref={post_urlRef}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor='description' className='form-label'>
+            Description
+          </label>
+          <input
+            type='text'
+            id='description'
+            className='form-input'
+            defaultValue={data[0].description ? data[0].description : ''}
+            disabled={isDisabled}
+            ref={descriptionRef}
+          />
+        </fieldset>
+
+        {isDisabled ? (
+          <>
+            <button className='btn' onClick={handleClick}>
+              Edit
+            </button>
+          </>
+        ) : (
+          <>
+            <button className='btn' onClick={refresh}>
+              Cancel
+            </button>
+            <button className='btn' onClick={handleSave}>
+              Save
+            </button>
+          </>
+        )}
+      </div>
     </>
+  ) : (
+    <div>Loading</div>
   );
 }
 
