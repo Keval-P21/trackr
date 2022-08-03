63fc24a043dc422bdcca6dcda0f5f443f3ac918e
Keval-P21 <keval.p21+github@gmail.com>
2022-08-03 07:49:06 +0100

    Docs Readme update, What? Tech Used?

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [currentUser]);
   // });
 
   async function getUserJobs(userId) {
--- client/src/components/eventstasks/EventsTasks.js
+++ client/src/components/eventstasks/EventsTasks.js
@@ -5,7 +5,7 @@ import JobTasks from '../todos/JobTasks';
 function EventsTasks({ jobs, events }) {
   // console.log(events);
   return jobs && events ? (
-    <section className='job-det-cont'>
+    <section className='task-event-cont'>
       <div className='form-box blue white-text'>
         <h2 className='white-text slim margin-bottom'>Tasks</h2>
         <div>
